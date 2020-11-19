c8c194cef6eed18b8cf7e6b4d4f81d26bd95f615
eduard-martinez <edfemagonza@gmail.com>
2020-11-19 17:28:37 -0500

    Horario de atencion 2

--- codes/Help (horario atención).R
+++ codes/Help (horario atención).R
@@ -31,21 +31,50 @@ lista_archivos = files[c(grep('Cabecera',files),grep('Resto',files))] %>%
 lista_data = lapply(lista_archivos, function(x) f_read(path = x))
 
 #### Crear objetos por modulo
-caracte_u = lista_data[1:12] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1)
-caracte_r = lista_data[13:24] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0)
+caracte_u = lista_data[grep('Cabecera - Cara',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1)
+caracte_r = lista_data[grep('Resto - Cara',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0)
 caracte = plyr::rbind.fill(caracte_u,caracte_r)
 
-desocupado = plyr::rbind.fill(lista_data[25:36] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1),
-                           lista_data[37:48] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0))
+desocupado = plyr::rbind.fill(lista_data[grep('Cabecera - Desocu',lista_archivos) ] %>% data.table::rbindlist(use.names = T,fill = T),
+                              lista_data[grep('Resto - Desocu',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T)) %>% 
+             dplyr::select(-hogar,-regis, -area, -clase, -mes, -dpto, -fex_c_2011)
 
-inactivo = plyr::rbind.fill(lista_data[49:60] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1),
-                           lista_data[61:72] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0))
+inactivo = plyr::rbind.fill(lista_data[grep('Cabecera - Inact',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T),
+                            lista_data[grep('Resto - Inact',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) ) %>% 
+           dplyr::select(-hogar,-regis, -area, -clase, -mes, -dpto, -fex_c_2011)
 
-ocupado = plyr::rbind.fill(lista_data[73:84] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1),
-                           lista_data[85:96] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0))
+ocupado = plyr::rbind.fill(lista_data[grep('Cabecera - Ocupado',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) ,
+                           lista_data[grep('Resto - Ocupado',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) )%>% 
+          dplyr::select(-hogar,-regis, -area, -clase, -mes, -dpto, -fex_c_2011)
 
-fuerza = plyr::rbind.fill(lista_data[97:108] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 1),
-                          lista_data[109:120] %>% data.table::rbindlist(use.names = T,fill = T) %>% mutate(urbano = 0))
+fuerza = plyr::rbind.fill(lista_data[grep('Cabecera - Fuerza',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) ,
+                          lista_data[grep('Resto - Fuerza',lista_archivos)] %>% data.table::rbindlist(use.names = T,fill = T) )%>% 
+         dplyr::select(-hogar,-regis, -area, -clase, -mes, -dpto, -fex_c_2011)
 
+#### Crear una base de datos
+geih = merge(caracte,desocupado,c('secuencia_p','orden','directorio'),all.x = T) %>% 
+       merge(.,ocupado,c('secuencia_p','orden','directorio'),all.x = T) %>% 
+       merge(.,fuerza,c('secuencia_p','orden','directorio'),all.x = T) %>% 
+       merge(.,inactivo,c('secuencia_p','orden','directorio'),all.x = T)
+saveRDS(file = 'data/procesada/GEIH 2019.rds',object = geih)         
 
+#### 1.4.
+rm(list = ls())
+geih = readRDS(file = 'data/procesada/GEIH 2019.rds') %>% 
+       subset(p6040>=18&p6040<=28)
+geih = mutate(geih , fex_c_2011 = as.character(fex_c_2011) %>% gsub(',','.',.) %>% as.numeric()) %>% 
+       mutate(peso = fex_c_2011/12)
+
+t_falbeta = geih %>% group_by(dpto,p6160) %>% summarise(n_p = sum(peso)) %>%
+            reshape2::dcast(.,dpto~p6160)
+colnames(t_falbeta) = c('dpto','sabe_le','no_sabe_le') 
+t_falbeta = mutate(t_falbeta , tasa = no_sabe_le/(no_sabe_le+sabe_le)*100)
+
+t_falbeta %>%
+  mutate(name = fct_reorder(as.character(dpto), tasa)) %>%
+  ggplot( aes(x=as.character(dpto), y=tasa)) +
+  geom_bar(stat="identity", fill="#f68060", alpha=.6, width=.4) +
+  coord_flip() +
+  xlab("") +
+  theme_bw()
 
